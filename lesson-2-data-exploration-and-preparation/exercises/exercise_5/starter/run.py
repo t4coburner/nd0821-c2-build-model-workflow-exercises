2886da59ca95119cf82042544d2ed158d35c1bcf
Victor Nguyen <victornguyen@chevron.com>
2026-08-24 20:22:26 -0500

    exercise 5 complete

--- lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import logging
+import os
 import pandas as pd
 import wandb
 
@@ -13,8 +14,28 @@ def go(args):
 
     run = wandb.init(project="exercise_5", job_type="process_data")
 
-    ## YOUR CODE HERE
-    pass
+    artifact = run.use_artifact(args.input_artifact)
+    df = pd.read_parquet(artifact.file())
+
+    df = df.drop_duplicates().reset_index(drop=True)
+
+    df['title'].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
+
+    output_file = "preprocessed_data.csv"
+    df.to_csv(output_file, index=False)
+
+    artifact = wandb.Artifact(
+        name=args.artifact_name,
+        type=args.artifact_type,
+        description=args.artifact_description
+    )
+    artifact.add_file(output_file, name=os.path.basename(output_file))
+
+    run.log_artifact(artifact)
+
+    artifact.wait()
 
 
 if __name__ == "__main__":
@@ -48,3 +69,11 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     go(args)
+
+"""
+mlflow run . \
+    -P input_artifact=exercise_4/genres_mod.parquet:latest \
+    -P artifact_name=preprocessed_data.csv \
+    -P artifact_type=preprocessed_data \
+    -P artifact_description="Preprocessed data with duplicates removed and text feature created"
+"""
